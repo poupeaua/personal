7335db00c3112e944840cccc63ef02787b9cce09
poupeaua <ale.poupeau@gmail.com>
2025-12-15 23:57:43 +0000

    Deploying to gh-pages from @ poupeaua/personal@36c083c68ad3f7cb98ba5fc90946134485085209 🚀

--- js/auto_current_year.js
+++ js/auto_current_year.js
@@ -1,6 +1,21 @@
 document.addEventListener('DOMContentLoaded', function() {
+    const curYear = new Date().getFullYear();
+    const birthYear = 1996;
+    const magicStartAge = 17;
+    const guitarStartAge = 15;
+
     const yearSpan = document.getElementById('currentYear');
     if (yearSpan) {
-      yearSpan.textContent = new Date().getFullYear();
+      yearSpan.textContent = curYear;
+    }
+
+    const yearsExpMagic = document.getElementById('yearsExpMagic');
+    if (yearsExpMagic) {
+      yearsExpMagic.textContent = curYear - birthYear - magicStartAge;
+    }
+
+    const yearsExpGuitar = document.getElementById('yearsExpGuitar');
+    if (yearsExpGuitar) {
+      yearsExpGuitar.textContent = curYear - birthYear - guitarStartAge;
     }
 });
